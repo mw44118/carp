2f099decff921da8d9ccfa5f6deacef76426f214
W. Matthew Wilson <matt@tplus1.com>
2012-11-04 15:47:07 -0500

    reorganized to use a CarpScript class

--- src/carp.py
+++ src/carp.py
@@ -13,7 +13,9 @@ import jinja2
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger('carp')
 
-class CarpLister(object):
+class CarpScript(object):
+
+    __metaclass__ = abc.ABCMeta
 
     @staticmethod
     def find_carpdir():
@@ -22,6 +24,13 @@ class CarpLister(object):
 
             if 'carpdir' in os.listdir(d):
                 return os.path.join(d, 'carpdir')
+
+    @abc.abstractmethod
+    def set_up_args(self):
+        raise NotImplementedError
+
+
+class CarpLister(CarpScript):
 
     def set_up_args(self):
 
@@ -58,7 +67,7 @@ class CarpLister(object):
             yield(tmpl)
 
 
-class CarpAdder(object):
+class CarpAdder(CarpScript):
 
     def set_up_args(self):
 
@@ -73,14 +82,6 @@ class CarpAdder(object):
 
         return ap.parse_args()
 
-    @staticmethod
-    def find_carpdir():
-
-        for d in clepy.walkup():
-
-            if 'carpdir' in os.listdir(d):
-                return os.path.join(d, 'carpdir')
-
     @classmethod
     def add_template(cls):
 
@@ -91,7 +92,6 @@ class CarpAdder(object):
         carpdir = args.carpdir or self.find_carpdir()
 
         self.copy_template(args.file_to_add, carpdir)
-
 
     def copy_template(self, file_to_add, carpdir):
 
@@ -104,16 +104,7 @@ class CarpAdder(object):
                 basename))
 
 
-class CarpRenderer(object):
-
-    @staticmethod
-    def find_carpdir():
-
-        for d in clepy.walkup():
-
-            if 'carpdir' in os.listdir(d):
-                return os.path.join(d, 'carpdir')
-
+class CarpRenderer(CarpScript):
 
     def set_up_args(self):
 
@@ -133,7 +124,6 @@ class CarpRenderer(object):
             help='This is the template to render')
 
         return ap.parse_args()
-
 
     @classmethod
     def render(cls):
@@ -162,3 +152,53 @@ class CarpRenderer(object):
         j = env.get_template(template)
 
         return j.render(**values)
+
+
+class CarpInfoGetter(CarpScript):
+
+    def set_up_args(self):
+
+        ap = argparse.ArgumentParser(
+            description='Render a stored template')
+
+        ap.add_argument('--carpdir',
+            help='Use this as the carp directory')
+
+        ap.add_argument('--define',
+            action='append',
+            metavar="key=value",
+            help='Define a value to use in the template, like '
+                'projname=foo.  Can use multiple times.')
+
+        ap.add_argument('template',
+            help='This is the template to render')
+
+        return ap.parse_args()
+
+    @classmethod
+    def render(cls):
+
+        self = cls()
+
+        args = self.set_up_args()
+
+        carpdir = args.carpdir or self.find_carpdir()
+
+        log.debug('args.define is {0}.'.format(args.define))
+
+        defined_values = dict([s.split('=') for s in args.define]) \
+        if args.define else {}
+
+        print(self.render_template(carpdir, args.template,
+            defined_values))
+
+    def render_template(self, carpdir, template, values):
+
+        env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(carpdir),
+            undefined=jinja2.StrictUndefined)
+
+        j = env.get_template(template)
+
+        return j.render(**values)
+
